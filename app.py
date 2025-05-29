d4532326b5ce7b83cddd08f47b5661e7dea455d0
Syed Ghazi <s.ghazi3@share.epsb.ca>
2025-05-29 10:57:20 -0600

    App.py changes

--- app.py
+++ app.py
@@ -11,21 +11,46 @@ app = Flask(
     template_folder=os.path.join('Front','pages')
 )
 
-@app.route('/')
+# Define routes for your HTML pages
+@app.route("/")
 def home():
-    return render_template('main.html')
+    return render_template("main.html")
 
-@app.route('/game')
+@app.route("/game")
 def game():
-    return render_template('game.html')
+    return render_template("game.html")
 
-@app.route('/learn')
+@app.route("/email")
+def email():
+    return render_template("email.html")
+
+@app.route("/forgot")
+def forgot():
+    return render_template("forgot.html")
+
+@app.route("/leaderboard")
+def leaderboard():
+    return render_template("leaderboard.html")
+
+@app.route("/learn")
 def learn():
-    return render_template('learn.html')
+    return render_template("learn.html")
 
-@app.route('/navbar')
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/navbar")
 def navbar():
-    return render_template('navbar.html')
+    return render_template("navbar.html")
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
